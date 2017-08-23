46c7b67291f36c64155aba1367dd38fd424fab08
Henrique Carrasco <henrique.carrasco@outlook.com>
2017-08-23 06:37:15 -0300

    cleaning code

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
-// app.use(express.json());       // to support JSON-encoded bodies
-// app.use(express.urlencoded()); // to support URL-encoded bodies
 
 // view engine setup
 app.engine('html', cons.swig);
